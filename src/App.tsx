d454501cc37998d86365bff47b1221a1d2ec4fca
Tima Kreac <tkreac@gmail.com>
2021-09-29 21:36:01 +0600

    check if user is auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Layout } from 'antd'
 
 import AppRouter from './components/AppRouter'
 import Navbar from './components/Navbar'
 import './App.css'
+import { useActions } from './hooks/useActions'
+import { IUser } from './models/IUser'
 
 function App() {
+  const { setIsAuth, setUser } = useActions()
+
+  useEffect(() => {
+    if (localStorage.getItem('auth')) {
+      setUser({ username: localStorage.getItem('username') || '' } as IUser)
+      setIsAuth(true)
+    }
+  }, [])
+
   return (
     <Layout>
       <Navbar />
